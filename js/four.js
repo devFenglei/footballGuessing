c1123a856942f0af7b2c7aa6262f32c196572782
冯磊 <1435631337@qq.com>
2018-06-12 16:23:37 +0800

    12

--- js/four.js
+++ js/four.js
@@ -45,6 +45,34 @@ $(function(){
         }
         $('.right-score').html(newRightScore);
     });
+    var Ajax={
+        get: function(url, fn) {
+            // XMLHttpRequest对象用于在后台与服务器交换数据   
+            var xhr = new XMLHttpRequest();            
+            xhr.open('GET', url, true);
+            xhr.onreadystatechange = function() {
+                // readyState == 4说明请求已完成
+                if (xhr.readyState == 4 && xhr.status == 200 || xhr.status == 304) { 
+                    // 从服务器获得数据 
+                    fn.call(this, xhr.responseText);  
+                }
+            };
+            xhr.send();
+        },
+        // datat应为'a=a1&b=b1'这种字符串格式，在jq里如果data为对象会自动将对象转成这种字符串格式
+        post: function (url, data, fn) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", url, true);
+            // 添加http头，发送信息至服务器时内容编码类型
+            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");  
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 304)) {
+                    fn.call(this, xhr.responseText);
+                }
+            };
+            xhr.send(data);
+        }
+    }
     $('.submit-con').on('click',function(){
         let userName = $('#userName').val();
         let tel = $('#tel').val();
@@ -52,21 +80,25 @@ $(function(){
         if(tel.length < 11){
             alert('请您完善电话号码！');
         }else{
-            $.ajax({
-                url: 'http://120.78.68.167:8080/man/update?t='+new Date().getTime(),
-                type: 'get',
-                dataType: 'json',
-                data: {
-                    name: userName,
-                    phoneNum: tel,
-                    score: score
-                },
-                success: function(result){
-                    if(result){
-                        alert('提交成功');
-                    }
-                }
+            Ajax.get('http://120.78.68.167:8080/man/update?name='+userName+'&phoneNum='+tel+'&score='+score+'t='+new Date().getTime(),function(result){
+                console.log(result);
+                alert('提交成功');
             });
+            // $.ajax({
+            //     url: 'http://120.78.68.167:8080/man/update?t='+new Date().getTime(),
+            //     type: 'get',
+            //     dataType: 'json',
+            //     data: {
+            //         name: userName,
+            //         phoneNum: tel,
+            //         score: score
+            //     },
+            //     success: function(result){
+            //         if(result){
+            //             alert('提交成功');
+            //         }
+            //     }
+            // });
         }
     });
 });
