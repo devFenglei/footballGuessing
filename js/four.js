10ac028eb8cd55278e0c83910483f2de68bc3f13
冯磊 <1435631337@qq.com>
2018-06-12 14:23:10 +0800

    2323

--- js/four.js
+++ js/four.js
@@ -53,7 +53,7 @@ $(function(){
             alert('请您完善电话号码！');
         }else{
             $.ajax({
-                url: 'http://120.78.68.167:8080/man/update',
+                url: 'http://120.78.68.167:8080/man/update?t='+new Date().getTime(),
                 type: 'get',
                 dataType: 'json',
                 data: {
